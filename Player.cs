d8f12bf0142deaaea1828d400ef3b9fcccba484e
JairoWarner <91611684+JairoWarner@users.noreply.github.com>
2026-09-08 23:31:07 +0200

    Add Player

---
+++ Player.cs
@@ -0,0 +1,33 @@
+﻿class Player
+{
+    public string Name;
+    public int CurrentHitPoints;
+    public int MaximumHitPoints;
+    public Weapon CurrentWeapon;
+    public Location CurrentLocation;
+
+    public Player(string name, int currenthitpoints, int maximumhitpoints)
+    {
+        Name = name;
+        CurrentHitPoints = currenthitpoints;
+        MaximumHitPoints = maximumhitpoints;
+        CurrentWeapon = null;
+        CurrentLocation = null;
+    }
+
+    public bool IfAlive()
+    {
+        return CurrentHitPoints > 0;
+    }
+
+    public void Heal(int amount)
+    {
+        CurrentHitPoints += amount;
+        if (CurrentHitPoints > MaximumHitPoints)
+        {
+            CurrentHitPoints = MaximumHitPoints;
+        }
+    }
+
+
+}
