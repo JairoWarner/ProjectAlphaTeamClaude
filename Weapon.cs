185b5bcf60e99be7fb96c8677f9507a9c1c39f06
“justinpinas“ <1125198@hr.nl>
2026-09-09 14:01:26 +0200

    hoi

---
+++ Weapon.cs
@@ -0,0 +1,13 @@
+public class Weapon
+{
+    public int ID;
+    public string Name;
+    public int Damage;
+
+    public Weapon(int weaponId, string weaponName, int weaponDamage)
+    {
+        ID = weaponId;
+        Name = weaponName;
+        Damage = weaponDamage;
+    }
+}
