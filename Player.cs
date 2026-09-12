0f28949f478e893e726a78f6cc97e12e5708414e
TariqLV <tariqchavez@hotmail.com>
2026-09-13 01:31:08 +0200

    Added the function to move around to Player.cs Created the general layout of the map which will be shown whenever a player decides to move around, able to be added to its own menu function Added Program.cs (Empty for now)

--- Player.cs
+++ Player.cs
@@ -29,6 +29,113 @@ public class Player
         return World.RandomGenerator.Next(0, CurrentWeapon.Damage + 1);
     }
 
+    public void MoveLocations(Location CurrentLocation)
+    {
 
+        bool moving = false;
+        string moving_to = "";
+        string confirmation = "";
+        CurrentLocation.ShowMap();
+        Console.WriteLine($"You are currently at {CurrentLocation.Name}");
+        while(moving == false && moving_to != "Q")
+        {
+            Console.WriteLine("Which direction would you like to head in? (N/E/S/W) (Q to stay where you are.)");
+            moving_to = Console.ReadLine().ToUpper();
+            switch (moving_to)
+            {
+                case "N":
+                    if(CurrentLocation.LocationToNorth == null)
+                    {
+                        Console.WriteLine("Unfortunately there is no location to your North.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You're planning to go North, towards {CurrentLocation.LocationToNorth}. Are you sure? (Y/N)");
+                        confirmation = Console.ReadLine().ToUpper();
+                        if(confirmation == "Y")
+                        {
+                            Console.WriteLine($"You embark upon your path to {CurrentLocation.LocationToNorth}");
+                            CurrentLocation = CurrentLocation.LocationToNorth;
+                            moving = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You have decided to stick around for a little while longer");
+                        }
+                    }
+                    break;
+
+                case "E":
+                    if(CurrentLocation.LocationToEast == null)
+                    {
+                        Console.WriteLine("Unfortunately there is no location to your East.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You're planning to go East, towards {CurrentLocation.LocationToEast}. Are you sure? (Y/N)");
+                        confirmation = Console.ReadLine().ToUpper();
+                        if(confirmation == "Y")
+                        {
+                            Console.WriteLine($"You embark upon your path to {CurrentLocation.LocationToEast}");
+                            CurrentLocation = CurrentLocation.LocationToEast;
+                            moving = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You have decided to stick around for a little while longer");
+                        }
+                    }
+                    break;
+                case "S":
+                    if(CurrentLocation.LocationToSouth == null)
+                    {
+                        Console.WriteLine("Unfortunately there is no location to your South.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You're planning to go West, towards {CurrentLocation.LocationToSouth}. Are you sure? (Y/N)");
+                        confirmation = Console.ReadLine().ToUpper();
+                        if(confirmation == "Y")
+                        {
+                            Console.WriteLine($"You embark upon your path to {CurrentLocation.LocationToSouth}");
+                            CurrentLocation = CurrentLocation.LocationToSouth;
+                            moving = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You have decided to stick around for a little while longer");
+                        }
+                    }
+                    break;
+
+                case "W":
+                    if(CurrentLocation.LocationToWest == null)
+                    {
+                        Console.WriteLine("Unfortunately there is no location to your West.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You're planning to go West, towards {CurrentLocation.LocationToWest}. Are you sure? (Y/N)");
+                        confirmation = Console.ReadLine().ToUpper();
+                        if(confirmation == "Y")
+                        {
+                            Console.WriteLine($"You embark upon your path to {CurrentLocation.LocationToWest}");
+                            CurrentLocation = CurrentLocation.LocationToWest;
+                            moving = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You have decided to stick around for a little while longer");
+                        }
+                    }
+                    break;
+
+                case "Q":
+                    Console.WriteLine($"You've decided not to travel after all");
+                    break;
+
+            }
+        }
+    }
 
 }
