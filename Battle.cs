0b46556a58b379c722304af0cfa547adde12acb6
“justinpinas“ <1125198@hr.nl>
2026-09-14 10:24:06 +0200

    Menu toegevoegd

--- Battle.cs
+++ Battle.cs
@@ -4,21 +4,46 @@ public static class Battle
 {
     public static void Fight(Player player, Monster monster)
     {
+        bool hasFled = false;
         while (player.IfAlive() && monster.IfAlive()) // check of speler en monster nog leven
         {
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            int playerDamage = player.CalculateDamage();
-            monster.CurrentHitPoints -= playerDamage;
-            if (monster.CurrentHitPoints <0) monster.CurrentHitPoints = 0; // als het lager dan nul is word het veranderd naar nul
-            if (playerDamage > 0)
+            List<string> choiceMenu = new(["A", "F", "I"]);
+            string choice = " ";
+            while (!choiceMenu.Contains(choice.ToUpper()))
             {
-                Console.WriteLine($"You dealt {playerDamage} to the {monster.Name}!\nPlayer HP: {player.health.Currenthitpoints}\nMonster HP: {monster.CurrentHitPoints}");
+                Console.WriteLine("A: Attack");
+                Console.WriteLine("F: Flee");
+                Console.WriteLine("I: Inventory");
+                choice = Console.ReadLine();
             }
-            else
+            if (choice.ToUpper() == "A")
             {
-                Console.WriteLine($"You missed!\nPlayer HP: {player.health.Currenthitpoints}\nMonster HP: {monster.CurrentHitPoints}");
+                int playerDamage = player.CalculateDamage();
+                monster.CurrentHitPoints -= playerDamage;
+                if (monster.CurrentHitPoints < 0) monster.CurrentHitPoints = 0; // als het lager dan nul is word het veranderd naar nul
+
+                if (playerDamage > 0)
+                {
+                    Console.WriteLine($"You dealt {playerDamage} to the {monster.Name}!\nPlayer HP: {player.health.Currenthitpoints}\nMonster HP: {monster.CurrentHitPoints}");
+                }
+                else
+                {
+                    Console.WriteLine($"You missed!\nPlayer HP: {player.health.Currenthitpoints}\nMonster HP: {monster.CurrentHitPoints}");
+                }
             }
+            else if (choice.ToUpper() == "F")
+            {
+                if (player.health.Currenthitpoints >= 10)
+                {
+                    hasFled = true;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You need more HP to flee!");
+                }
+            }
+
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             if (monster.IfAlive()) // check of het monster nog leeft anders kan er aangevallen worden terwijl die al dood is
@@ -35,9 +60,13 @@ public static class Battle
                 }
             }
         }
-        if (player.IfAlive())
+        if (player.IfAlive() && hasFled == false)
         {
             Console.WriteLine($"You have defeated the {monster.Name}");
+        }
+        else if (player.IfAlive() && hasFled == true)
+        {
+            Console.WriteLine("You have fled!");
         }
         else
         {
