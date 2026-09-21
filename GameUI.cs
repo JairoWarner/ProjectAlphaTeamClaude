a9c1e6a3e17d5742e58bfac31577f1b2d9a3437b
Ivarvruijven <ivarvanruijven@gmail.com>
2026-09-21 21:02:26 +0200

    delete

--- GameUI.cs
+++
@@ -1,9 +0,0 @@
-public static class GameUI
-{
-    public static void ColorWriteLine(string text, ConsoleColor color)
-    {
-        Console.ForegroundColor = color;
-        Console.WriteLine(text);
-        Console.ResetColor();
-    }
-}
